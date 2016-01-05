ee237b0410023378c68dd54b2a5568a7ee78184d
konnbu <b1013143@fun.ac.jp>
2016-01-05 11:12:11 +0900

    仮完成？

--- Assets/Program/Cannon.js
+++ Assets/Program/Cannon.js
@@ -9,37 +9,64 @@ public var image : UnityEngine.UI.Image;
 private var count : int;
 private var hour : int;
 
+private var vol : float;
+
+private var csScript : GetVol;
+
+
 
 function Start () {
     // 画面中央の座標を取得。
-    center = Vector3(Screen.width/2, Screen.height/2, 10);
+    center = Vector3(Screen.width/2,Screen.height/2, 10);
     count=0;
+}
+
+function Awake(){
+    csScript = this.GetComponent("GetVol");
 }
 
 function Update () {
     hour = System.DateTime.Now.Hour;
-
-    var csScript = GetComponent("Getvol");
-
     if(6 <= hour && hour <18) prefab=ball;
     else prefab =syuriken;
-    if(Input.GetMouseButtonDown(0)){
+
+    vol = csScript.GetAveragedVolume();
+    image.transform.localScale = Vector3 ((count%6)*20, 15, 0);
+	
+    if(vol > 0.6){ //ゲージの増える音量の大きさ.
+        count++;
+        //image.transform.localScale = Vector3 ((count%6)*20, 15, 0);
+
+    }
+
+    if(count >= 1 && Input.GetMouseButtonDown(0)){
+        var bullet = LoadBullet();
+        count = count -1;
+        var ray : Ray = Camera.main.ScreenPointToRay(center);
+        var dir : Vector3 =ray.direction.normalized;
+        bullet.GetComponent.<Rigidbody>().velocity = dir * power;
+    }
+
+}
+
+/*    if(Input.GetMouseButtonDown(0)){
         count++;
         image.transform.localScale = Vector3 ((count%6)*20, 15, 0);
 
         if(count%6==0){
             var bullet = LoadBullet();
             var ray : Ray = Camera.main.ScreenPointToRay(center);
-            var dir : Vector3 = ray.direction.normalized;
-            bullet.GetComponent.<Rigidbody>().velocity = dir * power;
+            var dir : Vector3 =ray.direction.normalized;
+bullet.GetComponent.<Rigidbody>().velocity = dir * power;
         }
     }
 
-}
+}*/
 
-function LoadBullet() : GameObject{
-    var bullet = GameObject.Instantiate(prefab)as GameObject;
-    bullet.transform.parent = transform;
-    bullet.transform.localPosition = Vector3.zero;
-    return bullet;
-}
+    function LoadBullet() : GameObject{
+        var bullet = GameObject.Instantiate(prefab)as GameObject;
+        bullet.transform.parent = transform;
+        //bullet.transform.localPosition = Vector3((count%6)*20, 15, 0);
+        //bullet.transform.localPosition = Vector3.zero;
+        return bullet;
+    }
--- Assets/Standard Assets/GetVol.cs
+++ Assets/Standard Assets/GetVol.cs
@@ -9,26 +9,27 @@ using UnityEngine.UI;
 public class GetVol : MonoBehaviour
 {
     public static float a;
-    public Text volText;
+    //public Text volText;
 
     void Start()
     {
+        //volText.text = "Vol:0";
         // 空の Audio Sourceを取得
         var audio = GetComponent<AudioSource>();
         // Audio Source の Audio Clip をマイク入力に設定
         // 引数は、デバイス名（null ならデフォルト）、ループ、何秒取るか、サンプリング周波数
-        audio.clip = Microphone.Start(null, false, 10, 44100);
+        audio.clip = Microphone.Start(null, false, 3000, 44100);
         // マイクが Ready になるまで待機（一瞬）
         while (Microphone.GetPosition(null) <= 0) { }
 
 
         // 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
-        //audio.Play();
+        audio.Play();
 
     }
 
     //現フレームで再生されている AudioClip から平均的な音量を取得します.
-    float GetAveragedVolume()
+    public float GetAveragedVolume()
     {
         //AudioClip の情報を格納する配列.
         //256は適当です.少なすぎれば平均的なサンプルデータが得られなくなるかもしれず,
@@ -54,7 +55,7 @@ public class GetVol : MonoBehaviour
         //多すぎれば計算量が増えますので良い感じに...
         float[] data = new float[256];
         //最終的に返す音量データ.
-        float a = 0;
+        a = 0;
         //AudioClipからデータを抽出します.
         GetComponent<AudioSource>().GetOutputData(data, 0);
         //音データを絶対値に変換します.
@@ -63,7 +64,10 @@ public class GetVol : MonoBehaviour
             a += Mathf.Abs(s);
         }
 
-        GetAveragedVolume();
+        a = a / 256;
+        //volText.text = "Vol:" + a.ToString();
+
+        //GetAveragedVolume();
     }
 
 }
