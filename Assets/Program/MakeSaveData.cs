275be12fd5f8f51617ad5a1ff40efc1fde62807f
Kyohei <b1013162@fun.ac.jp>
2016-01-08 13:13:25 +0900

    syuusei

---
+++ Assets/Program/MakeSaveData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class MakeSaveData : MonoBehaviour { 
+	// Use this for initialization
+	void Start () {
+        if (System.IO.File.Exists("stage1.txt"))
+        {
+        }
+        else
+        {
+            StreamWriter sw;
+            FileInfo fi;
+            fi = new FileInfo("stage1.txt");
+            sw = fi.AppendText();
+            sw.WriteLine("0,0,0,0,0,");
+            sw.Close();
+        }
+
+        if (System.IO.File.Exists("stage2.txt"))
+        {
+        }
+        else
+        {
+            StreamWriter sw;
+            FileInfo fi;
+            fi = new FileInfo("stage2.txt");
+            sw = fi.AppendText();
+            sw.WriteLine("0,0,0,0,0,");
+            sw.Close();
+        }
+
+    }
+	
+}
