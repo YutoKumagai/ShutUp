bdaa13279ffcd0ab5b880a4c44ce4a9fd557dc31
Kyohei <b1013162@fun.ac.jp>
2016-05-16 15:05:16 +0900

    test2

---
+++ Assets/Program/ChangeView.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChangeView : MonoBehaviour {
+
+    public static int viewflag=0;
+    public Text Viewpoint;
+
+
+    public void ChangeButton()
+    {
+        if (viewflag % 2 == 0)
+        {
+            viewflag = viewflag+ 1;
+            Viewpoint.text = "ノーマル視点";
+        }
+        else
+        {
+             viewflag = viewflag + 1;
+             Viewpoint.text = "Y軸反転視点";
+        }
+    }
+    public static int ViewFlagPoint()
+    {
+        return viewflag;
+    }
+
+}
---
+++ Assets/Program/GetTime.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+public static var resulttime : int;
+public var TimeText : Text;
+
+function Start () {
+    resulttime = Timer.time;
+    TimeText.text = "時間：" + resulttime.ToString();
+}
